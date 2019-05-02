10a433b90f5cdd32100895d1e997c55e5f615741
Anh Nguyen Viet (DBS - DFC) <anhnv22@vpbank.com.vn>
2019-05-02 16:08:20 +0700

    Implement sass

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import classes from './Login.module.scss'
 
 class Login extends Component {
   render() {
     return(
-      <div>Login page</div>
+      <div className={classes.test}>Login page</div>
     )
   }
 }
